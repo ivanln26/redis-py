71b1dd504730154b0e9cc1b6b510150f660a1b97
Ivan Nunez <ivanluis.cba@gmail.com>
2019-11-26 14:35:01 -0300

    Second commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Recuperatorio de redis
+
+## Requirements
+
+* Python: 3.6
+* Redis: 5.0.7
+
+## Steps
+
+* Run docker containers
+
+```sh
+docker-compose up -d
+```
+
+* Create a virtualenviroment and then:
+
+```
+python3 read.py
+```
--- read.py
+++ read.py
@@ -1,11 +1,12 @@
 import redis
 import time
 
-r = redis.Redis(host='redis', port='6379', db=0)
+r = redis.Redis(host='localhost', port='6379', db=0)
 p = r.pubsub()
 p.subscribe('canal')
 
 while True:
     message = p.get_message()
-    print(message)
-    time.sleep(10)
+    if message:
+        print(message['data'])
+    time.sleep(1)
